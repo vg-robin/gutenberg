530c61a6aa76c30a09a5bd39132dfa08ceacf26c
George Mamadashvili <georgemamadashvili@gmail.com>
2025-02-10 08:40:25 +0400

    Query block: Add option to ignore sticky posts behavior (#69057)
    * Query block: Add option to ignore sticky posts behavior
    * Add backport changelog entry
    
    Unlinked contributors: keithdevon.
    
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: carolinan <poena@git.wordpress.org>
    Co-authored-by: markhowellsmead <markhowellsmead@git.wordpress.org>
    Co-authored-by: coreyworrell <coreyw@git.wordpress.org>

--- lib/compat/wordpress-6.8/blocks.php
+++ lib/compat/wordpress-6.8/blocks.php
@@ -222,15 +222,30 @@ add_filter( 'rest_pre_insert_wp_block', 'gutenberg_update_ignored_hooked_blocks_
  * Update Query `parents` argument validation for hierarchical post types.
  * A zero is a valid parent ID for hierarchical post types. Used to display top-level items.
  *
+ * Add new handler for `sticky` query argument.
+ *
  * @param array    $query The query vars.
  * @param WP_Block $block Block instance.
  * @return array   The filtered query vars.
  */
-function gutenberg_parents_query_vars_from_query_block( $query, $block ) {
+function gutenberg_update_query_vars_from_query_block_6_8( $query, $block ) {
 	if ( ! empty( $block->context['query']['parents'] ) && is_post_type_hierarchical( $query['post_type'] ) ) {
 		$query['post_parent__in'] = array_unique( array_map( 'intval', $block->context['query']['parents'] ) );
 	}
 
+	if ( isset( $block->context['query']['sticky'] ) && ! empty( $block->context['query']['sticky'] ) ) {
+		if ( 'ignore' === $block->context['query']['sticky'] ) {
+			$sticky = get_option( 'sticky_posts' );
+
+			/**
+			 * The core will set `post__not_in` because it asserts that any sticky value other than `only` is `exclude`.
+			 * Let's override that while supporting any `post__not_in` values outside sticky post logic.
+			 */
+			$query['post__not_in']        = array_diff( $query['post__not_in'], ! empty( $sticky ) ? $sticky : array() );
+			$query['ignore_sticky_posts'] = 1;
+		}
+	}
+
 	return $query;
 }
-add_filter( 'query_loop_block_query_vars', 'gutenberg_parents_query_vars_from_query_block', 10, 2 );
+add_filter( 'query_loop_block_query_vars', 'gutenberg_update_query_vars_from_query_block_6_8', 10, 2 );
