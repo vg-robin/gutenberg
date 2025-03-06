7f943f568d32c6b9f6d9e8877aa7afef8ee59286
George Mamadashvili <georgemamadashvili@gmail.com>
2025-03-06 13:48:53 +0400

    Editor: Refactor the 'PostVisibility' component (#69451)
    * PostVisibility: Don't immediately publish a post when changing status to 'private'
    * PostVisibility: Refactor to use 'RadioControl'
    
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: afercia <afercia@git.wordpress.org>

--- packages/editor/src/components/post-visibility/index.js
+++ packages/editor/src/components/post-visibility/index.js
@@ -4,9 +4,9 @@
 import { __ } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
 import {
-	VisuallyHidden,
-	__experimentalConfirmDialog as ConfirmDialog,
 	TextControl,
+	RadioControl,
+	__experimentalVStack as VStack,
 } from '@wordpress/components';
 import { useInstanceId } from '@wordpress/compose';
 import { useSelect, useDispatch } from '@wordpress/data';
@@ -15,7 +15,7 @@ import { __experimentalInspectorPopoverHeader as InspectorPopoverHeader } from '
 /**
  * Internal dependencies
  */
-import { visibilityOptions } from './utils';
+import { VISIBILITY_OPTIONS } from './utils';
 import { store as editorStore } from '../../store';
 
 /**
@@ -34,42 +34,26 @@ export default function PostVisibility( { onClose } ) {
 		password: select( editorStore ).getEditedPostAttribute( 'password' ),
 	} ) );
 
-	const { editPost, savePost } = useDispatch( editorStore );
+	const { editPost } = useDispatch( editorStore );
 
 	const [ hasPassword, setHasPassword ] = useState( !! password );
-	const [ showPrivateConfirmDialog, setShowPrivateConfirmDialog ] =
-		useState( false );
 
-	const setPublic = () => {
-		editPost( {
-			status: visibility === 'private' ? 'draft' : status,
-			password: '',
-		} );
-		setHasPassword( false );
-	};
+	function updateVisibility( value ) {
+		const nextValues = {
+			public: {
+				status: visibility === 'private' ? 'draft' : status,
+				password: '',
+			},
+			private: { status: 'private', password: '' },
+			password: {
+				status: visibility === 'private' ? 'draft' : status,
+				password: password || '',
+			},
+		};
 
-	const setPrivate = () => {
-		setShowPrivateConfirmDialog( true );
-	};
-
-	const confirmPrivate = () => {
-		editPost( { status: 'private', password: '' } );
-		setHasPassword( false );
-		setShowPrivateConfirmDialog( false );
-		savePost();
-	};
-
-	const handleDialogCancel = () => {
-		setShowPrivateConfirmDialog( false );
-	};
-
-	const setPasswordProtected = () => {
-		editPost( {
-			status: visibility === 'private' ? 'draft' : status,
-			password: password || '',
-		} );
-		setHasPassword( true );
-	};
+		editPost( nextValues[ value ] );
+		setHasPassword( value === 'password' );
+	}
 
 	const updatePassword = ( value ) => {
 		editPost( { password: value } );
@@ -82,33 +66,13 @@ export default function PostVisibility( { onClose } ) {
 				help={ __( 'Control how this post is viewed.' ) }
 				onClose={ onClose }
 			/>
-			<fieldset className="editor-post-visibility__fieldset">
-				<VisuallyHidden as="legend">
-					{ __( 'Visibility' ) }
-				</VisuallyHidden>
-				<PostVisibilityChoice
-					instanceId={ instanceId }
-					value="public"
-					label={ visibilityOptions.public.label }
-					info={ visibilityOptions.public.info }
-					checked={ visibility === 'public' && ! hasPassword }
-					onChange={ setPublic }
-				/>
-				<PostVisibilityChoice
-					instanceId={ instanceId }
-					value="private"
-					label={ visibilityOptions.private.label }
-					info={ visibilityOptions.private.info }
-					checked={ visibility === 'private' }
-					onChange={ setPrivate }
-				/>
-				<PostVisibilityChoice
-					instanceId={ instanceId }
-					value="password"
-					label={ visibilityOptions.password.label }
-					info={ visibilityOptions.password.info }
-					checked={ hasPassword }
-					onChange={ setPasswordProtected }
+			<VStack spacing={ 4 }>
+				<RadioControl
+					label={ __( 'Visibility' ) }
+					hideLabelFromVision
+					options={ VISIBILITY_OPTIONS }
+					selected={ hasPassword ? 'password' : visibility }
+					onChange={ updateVisibility }
 				/>
 				{ hasPassword && (
 					<TextControl
@@ -123,44 +87,7 @@ export default function PostVisibility( { onClose } ) {
 						maxLength={ 255 }
 					/>
 				) }
-			</fieldset>
-			<ConfirmDialog
-				isOpen={ showPrivateConfirmDialog }
-				onConfirm={ confirmPrivate }
-				onCancel={ handleDialogCancel }
-				confirmButtonText={ __( 'Publish' ) }
-				size="medium"
-			>
-				{ __( 'Would you like to privately publish this post now?' ) }
-			</ConfirmDialog>
-		</div>
-	);
-}
-
-function PostVisibilityChoice( { instanceId, value, label, info, ...props } ) {
-	return (
-		<div className="editor-post-visibility__choice">
-			<input
-				type="radio"
-				name={ `editor-post-visibility__setting-${ instanceId }` }
-				value={ value }
-				id={ `editor-post-${ value }-${ instanceId }` }
-				aria-describedby={ `editor-post-${ value }-${ instanceId }-description` }
-				className="editor-post-visibility__radio"
-				{ ...props }
-			/>
-			<label
-				htmlFor={ `editor-post-${ value }-${ instanceId }` }
-				className="editor-post-visibility__label"
-			>
-				{ label }
-			</label>
-			<p
-				id={ `editor-post-${ value }-${ instanceId }-description` }
-				className="editor-post-visibility__info"
-			>
-				{ info }
-			</p>
+			</VStack>
 		</div>
 	);
 }
