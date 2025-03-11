aaaf7855a723853c221c52c8498ebaf367636101
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2025-03-11 16:52:20 +0900

    Classic theme site preview: Make all interactive elements unclickable (#69522)
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: afercia <afercia@git.wordpress.org>

--- packages/edit-site/src/components/editor/site-preview.js
+++ packages/edit-site/src/components/editor/site-preview.js
@@ -4,6 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
+import { focus } from '@wordpress/dom';
 
 export default function SitePreview() {
 	const siteUrl = useSelect( ( select ) => {
@@ -34,10 +35,8 @@ export default function SitePreview() {
 					.getElementsByTagName( 'body' )[ 0 ]
 					.classList.remove( 'admin-bar' );
 				// Make interactive elements unclickable.
-				const interactiveElements = document.querySelectorAll(
-					'a, button, input, details, audio'
-				);
-				interactiveElements.forEach( ( element ) => {
+				const focusableElements = focus.focusable.find( document );
+				focusableElements.forEach( ( element ) => {
 					element.style.pointerEvents = 'none';
 					element.tabIndex = -1;
 					element.setAttribute( 'aria-hidden', 'true' );
