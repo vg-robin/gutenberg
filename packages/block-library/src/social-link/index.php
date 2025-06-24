6a3bb8d1915e36e83a1dd8279b259947807e1ecb
George Mamadashvili <georgemamadashvili@gmail.com>
2025-06-24 09:48:50 +0400

    Social: Allow custom link icons using block variations (#70261)
    Unlinked contributors: mkaz, KoiKoneessa, mkkeck, YogieAnamCara, aksthelion, bshuchter, davidallenlewis, chs-kasvu, hundkurserforalla, thekamilpro, aut0poietic.
    
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: gziolo <gziolo@git.wordpress.org>
    Co-authored-by: bph <bph@git.wordpress.org>
    Co-authored-by: costasovo <costasovo@git.wordpress.org>
    Co-authored-by: strarsis <strarsis@git.wordpress.org>
    Co-authored-by: jasmussen <joen@git.wordpress.org>
    Co-authored-by: talldan <talldanwp@git.wordpress.org>
    Co-authored-by: apmeyer <apmeyer@git.wordpress.org>
    Co-authored-by: Drivingralle <drivingralle@git.wordpress.org>
    Co-authored-by: phil-sola <philsola@git.wordpress.org>
    Co-authored-by: nurianadal <sedmikrasky@git.wordpress.org>
    Co-authored-by: daveguitaruno <daveguitaruno@git.wordpress.org>
    Co-authored-by: 0aveRyan <dryanpress@git.wordpress.org>
    Co-authored-by: gaambo <gaambo@git.wordpress.org>

--- packages/block-library/src/social-link/index.php
+++ packages/block-library/src/social-link/index.php
@@ -335,6 +335,19 @@ function block_core_social_link_services( $service = '', $field = '' ) {
 		),
 	);
 
+	/**
+	 * Filter the list of available social service.
+	 *
+	 * This can be used to change icons or add custom icons (additionally to variations in the editor).
+	 * Icons should be directly renderable - therefore SVGs work best.
+	 *
+	 * @since 6.9.0
+	 *
+	 * @param array $services_data The list of services. Each item is an array containing a 'name' and 'icon' key.
+	 * @return array The list of social services.
+	 */
+	$services_data = apply_filters( 'block_core_social_link_get_services', $services_data );
+
 	if ( ! empty( $service )
 		&& ! empty( $field )
 		&& isset( $services_data[ $service ] )
